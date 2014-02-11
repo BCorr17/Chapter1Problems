1e935e7807bece8fc095d38708ca48212fbcde33
BCorr17 <CorrBradley@gmail.com>
2014-02-11 10:03:12 -0500

    Save2

---
+++ Chapter1Projects/Initials/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Initials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BBBBBBBBBBB                 CCCCCCCCCCCCCC  ");
+            Console.WriteLine("BBBBBBBBBBBB              CCCCCCCCCCCCC     ");
+            Console.WriteLine("BB       BBBB            CCCCCCCCCCC         ");
+            Console.WriteLine("BB       BBBB           CCCCCCC             ");
+            Console.WriteLine("BB    BBBBBBB           CCCCC             ");
+            Console.WriteLine("BB   BBBBBBB            CCCCC             ");
+            Console.WriteLine("BB    BBBBBB            CCCCCC             ");
+            Console.WriteLine("BB       BBBB           CCCCCCC             ");
+            Console.WriteLine("BB       BBBB            CCCCCCCCCCC         ");
+            Console.WriteLine("BBBBBBBBBBBB              CCCCCCCCCCCCC     ");
+            Console.WriteLine("BBBBBBBBBBB                 CCCCCCCCCCCCCC  ");
+        }
+    }
+}
